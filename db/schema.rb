dad67081354916984cc990573d4412ae69c4c75c
Loc <nguyengialoc7@gmail.com>
2023-02-04 06:59:08 +0000

    add book detail

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_03_094411) do
+ActiveRecord::Schema.define(version: 2023_02_04_065604) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
